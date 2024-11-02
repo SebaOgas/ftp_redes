21b01887d0b5a5f1543adbd7c904cd09a0f312b0
Facundo <facundojulianmaya@gmail.com>
2024-11-02 19:24:54 -0300

    Cliente FTP terminado
    Comandos básicos implementados

--- cliente.py
+++ cliente.py
@@ -4,33 +4,41 @@ Implementar una aplicación cliente de FTP, con las siguientes características:
     Se debe poder subir y bajar archivos desde un servidor de FTP.
 """
 import ftplib
+import string
 import sys
 import os
+import random
+from getpass import getpass
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
 cls()
 
-if len(sys.argv) < 1:
-    raise Exception("Ingrese la IP")
+def lcd(dir):
+    os.system(f'cd {dir}' if os.name=='nt' else f'cd {dir}')
+
+if len(sys.argv) < 2:
+    print("Error: Ingrese la IP")
+    exit()
 
 ip = sys.argv[1]
-port = int(sys.argv[2])
+port = 21
 
-if port is None:
-    port = 21
+if len(sys.argv) >= 3:
+    port = int(sys.argv[2])
 
 with ftplib.FTP() as ftp:
     # conexión al servidor local
     ftp.connect(ip, port)
+    print("Conectado usando IP: "+ip+" y puerto: "+str(port))
     
     # obtener mensaje de bienvenida
     print(ftp.getwelcome())
 
     # identificación
     user = input("Usuario: ")
-    pw = input("Contraseña: ")
+    pw = getpass("Contraseña: ")
     ret_login = ""
     end = False
 
@@ -51,44 +59,73 @@ with ftplib.FTP() as ftp:
             match cmd:
                 case "help":
                     print("Comandos: ")
-                    print("ls: Listar contenidos del directorio actual")
+                    print("exit: Cerrar conexión al servidor")
+                    print("cls|clear: Limpiar consola")
+                    print("ls: Listar contenidos del directorio remoto actual")
+                    print("lls: Listar contenidos del directorio local actual")
+                    print("cd <nombre_dir>: Cambiar directorio remoto actual")
+                    print("lcd <nombre_dir>: Cambiar directorio local actual")
+                    print("mkdir <nombre_dir>: Crear directorio remoto")
+                    print("rmdir <nombre_dir>: Borrar directorio remoto")
+                    print("get|pull <nombre_archivo_remoto> [nombre_archivo_local]: Obtener archivo remoto")
+                    print("put|push <nombre_archivo_local> [nombre_archivo_remoto]: Subir archivo local")
+                    print("rm|delete <nombre_archivo>: Borrar archivo remoto")
                 case "exit":
                     end = True
-                case "clear":
+                case "clear"|"cls":
                     cls()
+
+                #Comandos remotos
                 case "ls":
                     print(ftp.retrlines('LIST'))
                 case "cd":
                     if len(words) != 2:
                         raise Exception("Debe ingresar la ruta del directorio")
-                    ftp.cwd(words[1])
+                    print(ftp.cwd(words[1]))
+                case "mkdir":
+                    if len(words) != 2:
+                        raise Exception("Debe ingresar el nombre del directorio a crear")
+                    print(ftp.mkd(words[1]))
+                case "rmdir":
+                    if len(words) != 2:
+                        raise Exception("Debe ingresar el nombre del directorio a borrar")
+                    print(ftp.rmd(words[1]))
+                case "get"|"pull":
+                    if len(words) < 2:
+                        raise Exception("Debe ingresar el nombre del archivo a obtener")
+                    if len(words) > 3:
+                        raise Exception("Arg esperados: <archivo_a_obtener> [nombre_de_archivo_local]")
+                    tmpstr = ''.join(random.choices(string.ascii_letters, k=12))
+                    with open(tmpstr, 'wb') as file:
+                        print(ftp.retrbinary("RETR "+words[1], lambda data: file.write(data)))
+                    if len(words) == 2:
+                        os.rename(tmpstr, words[1])
+                    if len(words) == 3:
+                        os.rename(tmpstr, words[2])
+                case "put"|"push":
+                    if len(words) < 2:
+                        raise Exception("Debe ingresar el nombre del archivo a subir")
+                    if len(words) > 3:
+                        raise Exception("Arg esperados: <archivo_a_subir> [nombre_de_archivo_remoto]")
+                    with open(words[1], 'rb') as file:
+                        if len(words)==2:
+                            print(ftp.storbinary('STOR '+words[1], file))
+                        else:
+                            print(ftp.storbinary('STOR '+words[2], file))
+                case "rm"|"delete":
+                    if len(words) < 2:
+                        raise Exception("Debe ingresar el nombre del archivo a borrar")
+                    print(ftp.delete(words[1]))
+
+                #Comandos locales
+                case "lcd":
+                    if len(words) != 2:
+                        raise Exception("Debe ingresar el nombre del directorio local")
+                    lcd(words[1])
+                    print("Directorio actual: "+words[1])
+                case "lls":
+                    for item in os.listdir():
+                        print(item)
+
         except Exception as e:
             print(e)
-
-
-            
-
-
-
-    """ # crear directorio
-    ftp.mkd(f'ejemplo_1')
-    print('Creado directorio ejemplo_1\n')
-    
-    # cambiar de directorio
-    ftp.cwd('/ejemplo_1/')
-    print('Cambiar a directorio ejemplo_1\n')
-
-    #subir un fichero de texto
-    with open('test.txt', 'rb') as txt_file:
-        ftp.storbinary('STOR prueba_up.txt', txt_file)
-
-    # ver directorio
-    print(ftp.retrlines('LIST'))
-
-    # cambiar de directorio
-    ftp.cwd('/')
-    print('Cambiar a directorio raíz\n')
-
-    # ver directorio
-    print(ftp.retrlines('LIST'))
- """
